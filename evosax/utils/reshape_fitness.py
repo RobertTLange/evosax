fbb028a74c64eec61512faa1eaadc64e712336c8
RobertTLange <robertlange0@gmail.com>
2021-09-16 19:29:43 +0200

    Add fitness reshaper class

--- evosax/utils/reshape_fitness.py
+++ evosax/utils/reshape_fitness.py
@@ -1,14 +1,14 @@
+import jax
 import jax.numpy as jnp
-from jax import jit
 
 
-@jit
+@jax.jit
 def z_score_fitness(fitness):
     """ Make fitness 'Gaussian' by substracting mean and dividing by std. """
     return (fitness - jnp.mean(fitness)) / jnp.std(fitness)
 
 
-@jit
+@jax.jit
 def rank_shaped_fitness(x, fitness, fit_range=[-0.5, 0.5]):
     """ REINFORCE weights scaled between [-1, 1] - variance reduction. """
     # Sort new results, extract elite, store best performer
@@ -19,3 +19,38 @@ def rank_shaped_fitness(x, fitness, fit_range=[-0.5, 0.5]):
     shaped_fitness = jnp.linspace(fit_range[0],
                                   fit_range[1], x.shape[0])
     return sorted_solutions, shaped_fitness
+
+
+class FitnessShaper(object):
+    def __init__(self, rank_fitness: bool = True,
+                 weight_decay: float = 0.01):
+        self.weight_decay = weight_decay
+        self.rank_fitness = rank_fitness
+
+    @partial(jax.jit, static_argnums=(0,))
+    def apply(self, x, fitness):
+        """ Apply weight decay and rank shaping. """
+        fitness_trafo = jax.lax.select(self.rank_fitness,
+                                       compute_centered_ranks(fitness),
+                                       fitness)
+        l2_fitness_reduction = - self.weight_decay * compute_weight_norm(x)
+        return fitness_trafo + l2_fitness_reduction
+
+
+def compute_ranks(fitness):
+    """ Return ranks in [0, len(fitness)). """
+    ranks = jnp.arange(len(fitness))[fitness.argsort()]
+    return ranks
+
+
+def compute_centered_ranks(fitness):
+    """ Return ~ -0.5 to 0.5 centered ranks. """
+    y = compute_ranks(fitness)
+    y /= (fitness.size - 1)
+    y -= .5
+    return y
+
+
+def compute_weight_norm(x):
+    """ Compute L2-norm of weights. Assumes x to be (popsize, num_dims). """
+    return jnp.mean(x * x, axis=1)
