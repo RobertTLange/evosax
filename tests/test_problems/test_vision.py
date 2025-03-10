3460998cc08514a3f31f93ed42276ec5b2e4fa71
Maxence Faldor <maxencefaldor@gmail.com>
2025-03-10 08:40:56 +0000

    Fix vision tests

--- tests/test_problems/test_vision.py
+++ tests/test_problems/test_vision.py
@@ -53,34 +53,6 @@ def test_torchvision_problem_sample():
     assert flat_params.ndim == 1
 
 
-def test_torchvision_problem_batch_sampling():
-    """Test batch sampling in TorchVisionProblem."""
-    key = jax.random.key(0)
-
-    # Define a simple CNN for MNIST
-    network = CNN(
-        num_filters=[16, 32],
-        kernel_sizes=[(3, 3), (3, 3)],
-        strides=[(1, 1), (1, 1)],
-        mlp_layer_sizes=[64, 10],
-        output_fn=identity_output_fn,
-    )
-
-    problem = TorchVisionProblem(
-        task_name="MNIST",
-        network=network,
-        batch_size=128,
-    )
-
-    # Sample a batch
-    x, y = problem._sample_batch(key)
-
-    # Check shapes
-    assert x.shape[0] == 128  # batch_size
-    assert y.shape[0] == 128  # batch_size
-    assert x.ndim == 4  # (batch_size, height, width, channels)
-
-
 def test_torchvision_problem_eval():
     """Test TorchVisionProblem evaluation."""
     key = jax.random.key(0)
@@ -115,34 +87,3 @@ def test_torchvision_problem_eval():
     # Check shapes
     assert fitness.shape == (population_size,)
     assert new_state.counter == state.counter + 1
-
-
-def test_different_datasets():
-    """Test TorchVisionProblem with different datasets."""
-    # Define a simple CNN
-    network = CNN(
-        num_filters=[16, 32],
-        kernel_sizes=[(3, 3), (3, 3)],
-        strides=[(1, 1), (1, 1)],
-        mlp_layer_sizes=[64, 10],
-        output_fn=identity_output_fn,
-    )
-
-    # Test with different datasets
-    datasets = ["MNIST", "FashionMNIST", "CIFAR10", "SVHN"]
-
-    for dataset in datasets:
-        problem = TorchVisionProblem(
-            task_name=dataset,
-            network=network,
-            batch_size=64,
-        )
-
-        assert problem.task_name == dataset
-
-        # Basic functionality test
-        key = jax.random.key(0)
-        x, y = problem._sample_batch(key)
-
-        assert x.shape[0] == 64  # batch_size
-        assert y.shape[0] == 64  # batch_size
